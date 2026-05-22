ebe1e42b4bbb1d278fbae29a8faabc004243dadd
Pracoder2546 <pratikaryan2011@gmail.com>
2026-05-22 13:20:54 +0530

    Add PORT support for server configuration
    Added PORT environment variable support and ensured server runs on it.

--- server.js
+++ server.js
@@ -97,9 +97,12 @@ io.on("connection", socket => {
     });
 });
 
-http.listen(3000, () => {
+const PORT =
+    process.env.PORT || 3000;
+
+http.listen(PORT, () => {
 
     console.log(
-        "RUNNING ON 3000"
+        "RUNNING"
     );
 });
